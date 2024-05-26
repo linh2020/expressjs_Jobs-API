80de5d4f9dc6f70cd1db14132ea3ba28ea76707d
linh2020 <cdev.linh@gmail.com>
2024-05-26 02:53:14 -0700

    Hash User Password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,21 @@
 const User = require("../models/user");
 const { StatusCodes } = require("http-status-codes");
 const { BadRequestError } = require("../errors/index");
+const bcrypt = require("bcryptjs");
 
 const register = async (req, res, next) => {
   const { name, email, password } = req.body;
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  const tempUser = { name, email, password: hashedPassword };
+
+  // Optional
+  // const { name, email, password } = req.body;
   // if (!name || !email || !password) {
   //   throw new BadRequestError("Please provide name, email and password");
   // }
 
-  const user = await User.create({ ...req.body });
+  const user = await User.create({ ...tempUser });
   res.status(StatusCodes.CREATED).json({ user });
 };
 

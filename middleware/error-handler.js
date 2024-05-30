8f6d6dab125194995def1acee2f6756d61ec6609
linh2020 <cdev.linh@gmail.com>
2024-05-30 00:54:08 -0700

    Duplicate Error - err.code === 11000

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -2,15 +2,23 @@ const { CustomAPIError } = require("../errors");
 const { StatusCodes } = require("http-status-codes");
 
 const errorHandlerMiddleware = (err, req, res, next) => {
-  // let customError = {
-  //   statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR, // 500
-  //   msg: err.message || "Something went wrong, please try again!",
-  // };
+  let customError = {
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR, // 500
+    msg: err.message || "Something went wrong, please try again!",
+  };
 
   if (err instanceof CustomAPIError) {
     return res.status(err.statusCode).json({ msg: err.message });
   }
 
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+  if (err.code && err.code === 11000) {
+    customError.msg = `Duplicate value entered for ${Object.keys(
+      err.keyValue
+    )} field, please choose another value`;
+    customError.statusCode = 400;
+  }
+
+  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+  return res.status(customError.statusCode).json({ msg: customError.msg });
 };
 module.exports = errorHandlerMiddleware;

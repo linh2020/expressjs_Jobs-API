b357205c1b410da611b472cbe83127662c11ae5c
linh2020 <cdev.linh@gmail.com>
2024-05-31 02:56:24 -0700

    Security Setup

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 require("dotenv").config();
 require("express-async-errors");
 
+// Security packages
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
 const express = require("express");
 const app = express();
 
@@ -20,7 +26,23 @@ const errorHandlerMiddleware = require("./middleware/error-handler");
 
 // parsing
 app.use(express.static("./public"));
+
+// Enable if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
+// see https://expressjs.com/en/guide/behind-proxies.html
+app.set("trust proxy", 1);
+
+// https://www.npmjs.com/package/express-rate-limit
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  })
+);
+
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // Routes
 app.use("/api/v1/auth", authRouter);
